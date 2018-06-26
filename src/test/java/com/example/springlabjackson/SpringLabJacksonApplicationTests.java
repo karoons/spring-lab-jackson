e78b59add60bfcefe95f653d9135ff463b490c09
ANANDA\karoon <karoon@ananda.co.th>
2018-06-26 13:22:14 +0700

    initial sourcecode

---
+++ src/test/java/com/example/springlabjackson/SpringLabJacksonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springlabjackson;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringLabJacksonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
