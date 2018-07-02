e9ee3ec741c908ebe52daa21d8ca846efd91ab2e
ANANDA\karoon <karoon@ananda.co.th>
2018-07-02 09:34:47 +0700

    Create Standalone test

--- src/main/java/com/ananda/labjackson/controller/HomeController.java
+++ src/main/java/com/ananda/labjackson/controller/HomeController.java
@@ -1,4 +1,29 @@
 package com.ananda.labjackson.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("home")
 public class HomeController {
+
+    @GetMapping("status")
+    public Object getServiceStatus() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("server_status", "ok");
+        return response;
+    }
+
+    @GetMapping("status/{id}")
+    public Object getServiceStatusByInstanceId(@PathVariable int id) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("server_status", "ok");
+        response.put("instance_id", "" + id);
+        return response;
+    }
 }
